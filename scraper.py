0d4fc833e59616662c5b044c9af2a8013b86b3ed
Gaetano <cesano.gaetano@gmail.com>
2022-12-15 10:38:08 +0100

    initial commit

--- scraper.py
+++ scraper.py
@@ -14,10 +14,8 @@ class JustEat:
         self.browser.get("https://www.justeat.com/")
         time.sleep(3)
 
-        #we take xpath of username div and write username send keys
         self.browser.find_element_by_class_name("Form_c-search-input_3ySg3").send_keys(address)
 
-        #we take xpath of loginbutton and click
         time.sleep(2)
 
 address = "Via Andrea Angiulli, 47, 70126, Bari"
