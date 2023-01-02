608f7c9aa71c526dcd5801500491a4612ed7b899
GabrieleConte <gabryconte90@gmail.com>
2023-01-02 20:25:10 +0100

    bugifix and accessibility enhancement

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ from flask import Flask, render_template, request, send_file, redirect, session,
 from flask_compress import Compress
 
 
-
-
 # QUI CI SARA' IL BACK END PER LA GESTIONE DEGLI INPUT
 
 
@@ -12,7 +10,7 @@ from flask_compress import Compress
 
 
 #Flask carica l'interfaccia(index.html), bisogna aggiustarla (css, html, js)
-compress=Compress()
+compress=Compress()#comprime i pacchetti per maggiori performance
 app = Flask(__name__)
 compress.init_app(app)
 app.secret_key = "key"
@@ -25,4 +23,5 @@ def index():
 def home():
     return render_template("index.html")
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0")
+    context=('cert.pem', 'key.pem')
+    app.run(debug=True)
